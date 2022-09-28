f45df1aa45ce02c1a6e6cee08b7a28d7b2946599
ProgBelarus <gngoog100@gmail.com>
2022-09-28 06:07:12 -0400

    BatchGCP with early stopping

--- src/MultivalidAlgorithms/GroupCoverage.py
+++ src/MultivalidAlgorithms/GroupCoverage.py
@@ -4,7 +4,16 @@ import jax.numpy as jnp
 import functools
 import optax
 
-def group_coverage(tau, x_train, y_train, num_groups, group_fn=(lambda x: [True]), epochs=50, batch_size=2048, lr=0.05):
+def group_coverage(tau, x_train, y_train, num_groups, group_fn=(lambda x: [True]), \
+                   batch_size=2048, lr=0.05, epochs=50, \
+                   opt_alg='sgd', sgd_momentum=0.5, sgd_nesterov=True, \
+                   early_stop=True, early_stop_epochs=10000, eps=0.001, rounds_without_improv=50, valid_size=0.2):
+
+    if early_stop:
+        from sklearn.model_selection import train_test_split
+        x_train, x_valid, y_train, y_valid = train_test_split(x_train, y_train, test_size=valid_size)
+        x_valid, y_valid = jnp.array(x_valid, dtype=jnp.int32), jnp.array(y_valid, dtype=jnp.float32)
+        epochs = early_stop_epochs
 
     x_train, y_train = jnp.array(x_train, dtype=jnp.int32), jnp.array(y_train, dtype=jnp.float32)
 
@@ -18,8 +27,12 @@ def group_coverage(tau, x_train, y_train, num_groups, group_fn=(lambda x: [True]
         y_pred = pred_fn_jax(theta, x)
         return jnp.mean(jax.lax.max(tau*(y_true - y_pred), (1-tau)*(y_pred - y_true)))
     
-    # for sgd: 0.1 lr, 0.5 momentum, nesterov=True, batch=2048, epochs=100
-    optimizer = optax.adam(lr)
+    if opt_alg == 'sgd':
+        optimizer = optax.sgd(learning_rate=lr, momentum=sgd_momentum, nesterov=sgd_nesterov)
+    elif opt_alg == 'adam':
+        optimizer = optax.adam(lr)
+    else:
+        optimizer = optax.sgd(learning_rate=0.05, momentum=0.5, nesterov=True)
 
     theta = jnp.zeros(num_groups)
     opt_state = optimizer.init(theta)
@@ -31,8 +44,14 @@ def group_coverage(tau, x_train, y_train, num_groups, group_fn=(lambda x: [True]
         theta = optax.apply_updates(theta, updates)
         return theta, opt_state
 
-    def TrainModelInBatches(x_train, y_train, epochs, theta, opt_state, batch_size=32):
+    def training_loop(x_train, y_train, x_valid, y_valid, epochs, theta, opt_state, batch_size):
         rng = np.random.RandomState(0)
+
+        if early_stop:
+            best_valid_loss = jnp.inf
+            rounds_without_improvement = 0
+            valid_loss_prev = jnp.inf
+        
         for i in range(1, epochs+1):
             inds = rng.permutation(len(x_train))
             x_train = x_train[inds]
@@ -46,9 +65,23 @@ def group_coverage(tau, x_train, y_train, num_groups, group_fn=(lambda x: [True]
 
                 theta, opt_state = batch_update(x_batch, y_batch, theta, opt_state)
 
+            if early_stop:
+                valid_loss_curr = pinball_loss_jax(theta, x_valid, y_valid)
+
+                rounds_without_improvement += 1
+                
+                if valid_loss_curr < best_valid_loss - eps:
+                    rounds_without_improvement = 0
+                if rounds_without_improvement >= rounds_without_improv:
+                    break
+                
+                if valid_loss_curr < best_valid_loss:
+                    best_valid_loss = valid_loss_curr
+                valid_loss_prev = valid_loss_curr
+                
         return theta
 
-    theta_opt = TrainModelInBatches(x_train, y_train, epochs=epochs, theta=theta, opt_state=opt_state, batch_size=batch_size)
+    theta_opt = training_loop(x_train, y_train, x_valid, y_valid, epochs, theta, opt_state, batch_size)
     print("\nOptimal theta found:", theta_opt)
 
     return (lambda x: np.dot(np.array(theta_opt), group_fn(x))), np.array(theta_opt)
